a719e7bfeee7f846bb78aa72590b7caf1d214ec8
Dhruv Barot <dhruvbarot892@gmail.com>
2025-10-31 22:18:07 -0400

    Toggle State Simplification in project dashboard

--- client/src/components/ProjectDashboard.tsx
+++ client/src/components/ProjectDashboard.tsx
@@ -3,70 +3,46 @@ import Board from "./Board";
 import List from "./List";
 
 function ProjectDashboard() {
-  const [summaryToggle, setSummaryToggle] = useState(false);
-  const [listToggle, setListToggle] = useState(false);
-  const [boardToggle, setBoardToggle] = useState(true);
+  const [view, setView] = useState("board"); // "board" or "list"
 
   return (
     <div className="pl-2">
-      <div>
+      <header>
         <h1 className="text-2xl font-bold">Project Name</h1>
         {/* <p className="text-sm text-gray-400">Description of the project</p> */}
-      </div>
-      <div className="flex">
-        {summaryToggle ? (
-          <h4 className="text-white">Summary</h4>
-        ) : (
-          <h4
-            // onClick={() => {
-            //   setSummaryToggle(true);
-            //   setListToggle(false);
-            //   setBoardToggle(false);
-            // }}
-            className="text-gray-500"
-          >
-            Summary
-          </h4>
+      </header>
+
+      <nav className="flex gap-3 mt-2">
+        <button
+          onClick={() => setView("list")}
+          className={`ml-2 ${
+            view === "list" ? "text-white" : "text-gray-500 hover:text-white"
+          }`}
+        >
+          List
+        </button>
+        <button
+          onClick={() => setView("board")}
+          className={`ml-2 ${
+            view === "board" ? "text-white" : "text-gray-500 hover:text-white"
+          }`}
+        >
+          Board
+        </button>
+      </nav>
+
+      <main className="mt-3">
+        {view === "board" && (
+          <div>
+            <Board />
+          </div>
         )}
-        {listToggle ? (
-          <h4 className="text-white ml-2">List</h4>
-        ) : (
-          <h4
-            onClick={() => {
-              setSummaryToggle(false);
-              setListToggle(true);
-              setBoardToggle(false);
-            }}
-            className="text-gray-500 ml-2"
-          >
-            List
-          </h4>
+        {view === "list" && (
+          <div>
+            <List />
+          </div>
         )}
-        {boardToggle ? (
-          <h4 className="text-white ml-2">Board</h4>
-        ) : (
-          <h4
-            onClick={() => {
-              setSummaryToggle(false);
-              setListToggle(false);
-              setBoardToggle(true);
-            }}
-            className="text-gray-500 ml-2"
-          >
-            Board
-          </h4>
-        )}
-      </div>
-      {boardToggle && (
-        <div>
-          <Board />
-        </div>
-      )}
-      {listToggle && (
-        <div>
-          <List />
-        </div>
-      )}
+      </main>
     </div>
   );
 }
