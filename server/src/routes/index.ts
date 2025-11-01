2321512521ff02821b62a277f002ab2b1aa69227
Dhruv Barot <dhruvbarot892@gmail.com>
2025-11-01 01:17:52 -0400

    temporary disabled middleware

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,35 +1,50 @@
-import { Router, type NextFunction, type Response, type Request } from "express";
+import {
+  Router,
+  type NextFunction,
+  type Response,
+  type Request,
+} from "express";
 import bugsRouter from "./bugRoutes.js";
 import userRouter from "./userRoutes.js";
 import commentsRouter from "./commentRoutes.js";
-import authRouter from "./authRoutes.js"
+import authRouter from "./authRoutes.js";
 import { CustomError } from "../errors/CustomError.js";
 import { auth } from "../middleware/auth.js";
 
 const router = Router();
 
-router.use("/auth", authRouter)
-router.use("/bugs", auth, bugsRouter);
-router.use("/users", auth, userRouter);
-router.use("/comments", auth, commentsRouter)
+router.use("/auth", authRouter);
+// router.use("/bugs", auth, bugsRouter);
+router.use("/bugs", bugsRouter);
+// router.use("/users", auth, userRouter);
+router.use("/users", userRouter);
+// router.use("/comments", auth, commentsRouter);
+router.use("/comments", commentsRouter);
 
 // Error middleware
-router.use((err: CustomError | Error, _req: Request, res: Response, _next: NextFunction) => {
-	console.error(err);
+router.use(
+  (
+    err: CustomError | Error,
+    _req: Request,
+    res: Response,
+    _next: NextFunction
+  ) => {
+    console.error(err);
 
-	if (err instanceof CustomError) {
-		res.status(err.statusCode).send({
-			name: err.name,
-			msg: err.message,
-			statusCode: err.statusCode,
-		});
-	} else {
-		res.status(500).send({
-			name: "InternalServerError",
-			msg: err.message || "An unexpected error occurred.",
-			statusCode: 500,
-		});
-	}
-});
+    if (err instanceof CustomError) {
+      res.status(err.statusCode).send({
+        name: err.name,
+        msg: err.message,
+        statusCode: err.statusCode,
+      });
+    } else {
+      res.status(500).send({
+        name: "InternalServerError",
+        msg: err.message || "An unexpected error occurred.",
+        statusCode: 500,
+      });
+    }
+  }
+);
 
 export default router;
