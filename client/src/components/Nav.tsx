0544358796a2b16f9a3b240bd4276c525b3448a7
Dhruv Barot <dhruvbarot892@gmail.com>
2025-11-12 20:55:44 -0500

    linked logo to "/"

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -3,13 +3,16 @@ import React from "react";
 import logo from "../assets/logo.svg";
 import Search from "./Search";
 import { Plus, User } from "lucide-react";
+import { Link } from "react-router-dom";
 
 export default function Nav() {
   return (
     <>
       <div className="flex pb-2 pl-2 pr-2 justify-between border-b-2 border-gray-700 content-cente items-center">
         <div>
-          <img src={logo} alt="Logo" width={50} height={50} />
+          <Link to="/">
+            <img src={logo} alt="Logo" width={50} height={50} />
+          </Link>
         </div>
         <div className="w-full flex items-center justify-center gap-2 ">
           <div className="w-2/3 ">
